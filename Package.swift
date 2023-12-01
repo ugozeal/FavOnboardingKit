d9be1bad4f88a1df161c882560a962d15864c3a3
David Okonkwo <okonkwodavidu@gmail.com>
2023-12-01 12:33:13 +0100

    first commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "FavOnboardingKit",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FavOnboardingKit",
             targets: ["FavOnboardingKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FavOnboardingKit"),
+            name: "FavOnboardingKit",
+            dependencies: ["SnapKit"]
+        ),
         .testTarget(
             name: "FavOnboardingKitTests",
             dependencies: ["FavOnboardingKit"]),
